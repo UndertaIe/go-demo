89ad3d32ed0272355b7577e179e8e03938423881
UndertaIe <n604712799@gmail.com>
2022-09-12 21:05:13 +0800

    m

--- go.mod
+++ go.mod
@@ -1,19 +1,15 @@
-module go-demo
+module github.com/UndertaIe/go-demo
 
 go 1.18
 
 require (
 	github.com/gomodule/redigo v1.8.9
-	github.com/spf13/cobra v1.5.0
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,53 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"go/importer"
-	"go/types"
-
-	"github.com/spf13/cobra"
-)
-
-var rootCmd = cobra.Command{}
+import "fmt"
 
 func main() {
-	defer func() {
-		rootCmd.Execute()
-	}()
-	pkg, err := importer.Default().Import("time")
-	if err != nil {
-		fmt.Printf("error: %s\n", err.Error())
-		return
-	}
-	scope := pkg.Scope()
-	for _, name := range scope.Names() {
-		if name == "Time" {
-			obj := scope.Lookup(name)
-			if tn, ok := obj.Type().(*types.Named); ok {
-				for i := 0; i < tn.NumMethods(); i++ {
-					fmt.Println()
-					m := tn.Method(i)
-					m.Pkg()
-					m.FullName()
-					m.Exported()
-				}
-				fmt.Printf("%#v\n", tn.NumMethods())
-			}
-		}
-	}
-}
-
-func AddDemos(funcs map[string]func()) {
-	for fn, f := range funcs {
-		AddDemo(fn, f)
-	}
-}
-
-func AddDemo(fn string, f func()) {
-	rootCmd.AddCommand(&cobra.Command{
-		Run: func(cmd *cobra.Command, args []string) {
-			f()
-		},
-		Use: fn,
-	})
+	fmt.Println("hello,world")
 }
