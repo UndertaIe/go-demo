350eae274520fcc6970a40a64172b2805d88b6f6
UndertaIe <n604712799@gmail.com>
2022-09-15 18:41:12 +0800

    recover demo

---
+++ demo/core/recover.go
@@ -0,0 +1,53 @@
+package core
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func (c Core) NestedRecover() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("NestedRecover")
+		}
+	}()
+	fmt.Println(1)
+	nestedFunc()
+} // nestedFunc panic: interface conversion: io.Reader is nil, not *os.File
+
+func nestedFunc() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("nestedFunc panic:", err)
+		}
+	}()
+	fmt.Println(2)
+	var reader io.Reader
+	fd := reader.(*os.File) // panic
+	fmt.Println(fd)
+}
+
+func (c Core) NestedRecoverFromRePanic() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("NestedRecover")
+		}
+	}()
+	fmt.Println(1)
+	nestedFuncFromRePanic()
+} //nestedFunc panic: interface conversion: io.Reader is nil, not *os.File
+// NestedRecover
+
+func nestedFuncFromRePanic() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("nestedFunc panic:", err)
+			panic(err)
+		}
+	}()
+	fmt.Println(2)
+	var reader io.Reader
+	fd := reader.(*os.File) // panic
+	fmt.Println(fd)
+}
