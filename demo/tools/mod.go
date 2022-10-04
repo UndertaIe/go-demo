1599eabfddb57b25f10733cd1c0e29e2fd654fde
UndertaIe <n604712799@gmail.com>
2022-10-04 16:01:42 +0800

    add demo

--- demo/tools/mod.go
+++ demo/tools/mod.go
@@ -11,7 +11,7 @@ import (
 // package未修改后项目名称，若是项目的package则不生效
 // replace github.com/getsentry/sentry-go v0.13.0 => github.com/UndertaIe/sentry-go v1.0.0   		 √√√√
 // replace github.com/getsentry/sentry-go/gin v0.13.0 => github.com/UndertaIe/sentry-go/gin v1.0.0   ××××
-// ！！！ go.mod没有依赖处理，当A项目导入库mod使用了replace，则A一样要使用replace
+// ！！！ go.mod没有依赖处理，当A项目导入B Module(mod使用了replace)，则A跟B一样要使用相同的replace
 func (c Tools) ReplaceOfMode() {
 	s, _ := strutil.Random("1234567890", 4)
 	fmt.Println("Random string", s, 20)
