6c1414e6706326a26fa8ddc014a98012d489a6b9
UndertaIe <n604712799@gmail.com>
2023-01-06 17:19:22 +0800

    add json

--- demo/lib/json.go
+++ demo/lib/json.go
@@ -18,3 +18,23 @@ func (Lib) Omitempty() {
 	s, _ = json.Marshal(t)
 	fmt.Println(string(s))
 }
+
+type A struct {
+	A string `json:"a"`
+	B string `json:"b"`
+}
+
+func (Lib) JsonSerialization() {
+	a := A{}
+	obj, _ := json.Marshal(a)
+	fmt.Println(string(obj))
+}
+
+// json反序列化时会去除结构体不存在的属性
+func (Lib) JsonDeserialization() {
+	obj := []byte(`{"a":"A","b":"B","c":"C"}`)
+	a := A{}
+	json.Unmarshal(obj, &a)
+	fmt.Println(obj)
+	fmt.Println(a)
+}
